8610697627c99b0bd4fe2ca69ea582dc393e3cf5
KuroP1 <55400967+KuroP1@users.noreply.github.com>
2023-01-17 16:43:54 +0800

    feat: added link items for nav

---
+++ components/LinkItem.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+interface LinkItemProps {
+  children: React.ReactNode;
+  href: string;
+}
+
+export const LinkItem = ({ children, href }: LinkItemProps) => {
+  return (
+    <Link
+      href={href}
+      className="flex items-center relative px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400 capitalize "
+    >
+      <li>{children}</li>
+    </Link>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,17 @@
 import Link from "next/link";
 import Image from "next/image";
+import { LinkItem } from "@/components";
 const index = () => {
   return (
     <div className="bg-gray-500 min-h-screen">
       <div className="relative px-4 sm:px-8 lg:px-12">
         <div id="nav" className="pt-8 flex flex-1  md:justify-evenly">
           <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-            <li className=" relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400">
-              <Link href="/">about</Link>
-            </li>
-            <li className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400">
-              <Link href="/">projects</Link>
-            </li>
-            <li className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400">
-              <Link href="/">timeline</Link>
-            </li>
-            <li className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400">
-              <Link href="/">interest</Link>
-            </li>
-            <li className=" relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400">
-              <Link href="/">socials</Link>
-            </li>
+            <LinkItem href="/"> About </LinkItem>
+            <LinkItem href="/"> Projects </LinkItem>
+            <LinkItem href="/"> timeline </LinkItem>
+            <LinkItem href="/"> interest </LinkItem>
+            <LinkItem href="/"> socials </LinkItem>
           </ul>
           <div className="flex justify-start">
             <Image
