a8fd3c8983004ff4af177c3bf2150b9b9f59d34c
KuroP1 <55400967+KuroP1@users.noreply.github.com>
2023-01-17 16:49:17 +0800

    wip: Color change

--- components/LinkItem.tsx
+++ components/LinkItem.tsx
@@ -9,7 +9,7 @@ export const LinkItem = ({ children, href }: LinkItemProps) => {
   return (
     <Link
       href={href}
-      className="flex items-center relative px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400 capitalize "
+      className="flex items-center relative px-3 py-2 transition hover:text-teal-100 dark:hover:text-teal-400 capitalize "
     >
       <li>{children}</li>
     </Link>
