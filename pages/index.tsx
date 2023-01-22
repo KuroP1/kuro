222da824990a49389fcf2cd0f61fbc8fc31f3f2c
KuroP1 <55400967+KuroP1@users.noreply.github.com>
2023-01-22 16:07:56 +0800

    wip: using swr to fetch data from apex api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
+import useSWR from "swr";
+
 import { LinkItem, Navbar, Footer } from "@/components";
 const index = () => {
   return (
     <div className="relative px-4 sm:px-8 lg:px-12">
       <Navbar />
+
       <Footer />
     </div>
   );
