1f773549b1731ca2c498b050b5071f9cd95502ab
KuroP1 <55400967+KuroP1@users.noreply.github.com>
2023-01-22 16:10:10 +0800

    wip:navbar update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import { LinkItem } from "./LinkItem";
 
 export const Navbar = () => {
   return (
-    <nav className="pt-8 flex items-center justify-between flex-col md:flex-row">
+    <nav className="pt-8 flex items-center justify-evenly flex-col md:flex-row">
       <ul className="order-2 md:order-1 flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <LinkItem href="/"> About </LinkItem>
         <LinkItem href="/"> Projects </LinkItem>
